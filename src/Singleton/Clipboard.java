c086825f81d0b2aadbf9a49597ac870455b1c23f
SupuniAbeysinghe <supuniab1@gmail.com>
2024-03-28 21:34:56 +0530

    singleton design pattern demonstration

--- src/Singleton/Clipboard.java
+++ src/Singleton/Clipboard.java
@@ -1,4 +1,24 @@
 package Singleton;
 
 public class Clipboard {
+    private static Clipboard instance =null;
+
+    private Clipboard(){
+
+    }
+    public static Clipboard getInstance(){
+        if (instance == null){
+            instance = new Clipboard();
+        }
+        return instance;
+    }
+
+    private String value;
+
+    public void copy(String data){
+        this.value =data;
+    }
+    public String paste(){
+        return this.value;
+    }
 }
